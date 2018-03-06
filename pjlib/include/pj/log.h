b1cb1951db3f8ef7137d8316e17f02cecc1df786
kenferguson <ken.ferguson@metaswitch.com>
2018-03-06 15:33:52 +0000

    Add support for ramrecorder to PJSIP

--- pjlib/include/pj/log.h
+++ pjlib/include/pj/log.h
@@ -95,8 +95,11 @@ enum pj_log_decoration
   static int trc_id = 0;                                                      \
   pj_log_ram_trace *pj_log_ramtrace_trace = pj_log_get_ram_trace_func();      \
   pj_log_ram_cache *pj_log_ramtrace_cache = pj_log_get_ram_cache_func();      \
-  (*pj_log_ramtrace_cache)(&trc_id,__FILE__,__LINE__,__VA_ARGS__);            \
-  (*pj_log_ramtrace_trace)(trc_id,__VA_ARGS__);                               \
+  if (pj_log_ramtrace_cache != NULL)                                          \
+  {                                                                           \
+    (*pj_log_ramtrace_cache)(&trc_id,__FILE__,__LINE__,__VA_ARGS__);          \
+    (*pj_log_ramtrace_trace)(trc_id,__VA_ARGS__);                             \
+  }                                                                           \
 }
 
 /**
@@ -118,8 +121,8 @@ enum pj_log_decoration
  * @hideinitializer
  */
 #define PJ_LOG(level,arg)	do { \
-                                    if (level <= 5) \
-                                        PJ_LOG_RAMTRACE arg; \
+            if (level <= 5)      \
+          PJ_LOG_RAMTRACE arg   \
 				    if (level <= pj_log_get_level()) \
 					pj_log_wrapper_##level(arg); \
 				} while (0)
@@ -169,12 +172,6 @@ PJ_DECL(void) pj_log_set_ram_trace_funcs( pj_log_ram_cache *cache,  pj_log_ram_t
  * @return	    Current RAM trace cache function.
  */
 PJ_DECL(pj_log_ram_cache*) pj_log_get_ram_cache_func(void);
-
-/**
- * Default RAM tracing callbacks used by front end logger function.
- */
-PJ_DECL(int) pj_log_ram_cache_dummy(const char *module, int lineno, const char*fmt, ...);
-PJ_DECL(void) pj_log_ram_trace_dummy(int trc_id, const char *fmt, ...);
 
 /**
  * Get the current RAM trace function that is used to trace log instances to a RAM buffer.
